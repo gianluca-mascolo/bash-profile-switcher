d17fb3113419cbaa124379d5b85cf3907f363783
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-09-16 20:49:11 +0200

    add first test snippet

---
+++ tests/profiles/snippets/vartest1.snippet.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+SNIPPET_NAME="vartest1"
+case "$1" in
+load)
+    export check_var_test1="test1 variable"
+    _snippet push "$SNIPPET_NAME" 2>/dev/null
+    ;;
+unload)
+    unset check_var_test1
+    _snippet pop "$SNIPPET_NAME" 2>/dev/null
+    ;;
+*)
+    true
+    ;;
+esac
+
+return 0
