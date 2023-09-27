b53dbfc09d188d244adb91cc860fadaaa71dd821
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-09-27 21:24:56 +0200

    modify snippets

--- snippets/kubecomp.sh
+++ snippets/kubecomp.sh
@@ -8,22 +8,13 @@
 # requires:
 #   - kubectl installed for command completion
 
-SNIPPET_NAME="$(basename "${BASH_SOURCE[0]}" .sh)"
 case "$1" in
 load)
-    _snippet search "$SNIPPET_NAME" 2>/dev/null && return 0
-    # load your settings here >>>
     hash kubectl && source <(kubectl completion bash)
-    # <<<
-    _snippet push "$SNIPPET_NAME" 2>/dev/null
     ;;
 unload)
-    _snippet search "$SNIPPET_NAME" 2>/dev/null || return 0
-    # unload your settings here >>>
     declare -F | while read -r funcname; do [[ "$funcname" =~ kubectl ]] && unset -f "$funcname"; done
     complete -p kubectl &>/dev/null && complete -r kubectl
-    # <<<
-    _snippet pop "$SNIPPET_NAME" 2>/dev/null
     ;;
 *)
     true
