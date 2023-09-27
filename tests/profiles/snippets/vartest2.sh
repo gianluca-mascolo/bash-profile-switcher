7267460093325963c66ec656a300d7a084318e21
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-09-27 20:58:36 +0200

    remove push from snippet

--- tests/profiles/snippets/vartest2.sh
+++ tests/profiles/snippets/vartest2.sh
@@ -1,15 +1,12 @@
 # shellcheck shell=bash
-SNIPPET_NAME="$(basename "${BASH_SOURCE[0]}" .sh)"
 case "$1" in
 load)
     export check_var_test2="test2 variable"
     export common_value="test2 common"
-    _snippet push "$SNIPPET_NAME" 2>/dev/null
     ;;
 unload)
     unset check_var_test2
     unset common_value
-    _snippet pop "$SNIPPET_NAME" 2>/dev/null
     ;;
 *)
     true
